ca6e9d263b8db55a1eb3ce971e753952c4d1162a
Gagan41 <gagant.p100@gmail.com>
2024-12-10 13:49:26 +0530

    Backend v0.0.1

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://discuza.in",
     credentials: true,
   })
 );
